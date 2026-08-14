bd1a94044bb57845b8a281ef59ec45ec310eb0ab
rodrigo-feitosa <rodrigooliveirafeitosa@gmail.com>
2026-08-13 21:15:18 -0300

    refactor: renomeação do model

---
+++ quickeats/app/Models/ProductCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_categories';
+
+    public function product()
+    {
+        return $this->hasMany(Product::class, 'id_categoria', 'id_categoria');
+    }
+}
